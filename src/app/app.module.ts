d77b122670262d54b727019e39359ce5eb668537
DaarkGms <lucaspiresrv@hotmail.com>
2023-06-23 00:53:36 -0300

    b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+        
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
